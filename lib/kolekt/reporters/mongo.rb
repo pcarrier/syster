50a84bc0be3c287a4379a2f2abdad4aac454a435
Pierre Carrier <pierre@gcarrier.fr>
2013-02-21 05:58:20 +0100

    Reporters::Mongo: fixed, seems to work :)

--- lib/kolekt/reporters/mongo.rb
+++ lib/kolekt/reporters/mongo.rb
@@ -5,34 +5,56 @@ require 'socket'
 module Kolekt::Reporters
   class Mongo < Base
     def self.runnable?
-      # Sadly mongodb doesn't allow '$' or '.' in key names.
-      # If somebody can be bothered to implement a decent workaround,
-      # I'll take patches.
-      return false
       Kolekt::Helpers::Require.can_require? %w[mongo]
     end
-  
+
     def initialize params={}
       require 'mongo'
 
-      @client = ::Mongo::MongoClient.new
-      @db = @client['kolekt']
-      @coll = @db['hosts']
+      @hostname = Socket.gethostname
+      @db = ::Mongo::MongoClient.new['kolekt']
 
-      @update = {:hostname => Socket.gethostname}
+      drys = @db['dry'].find(:hostname => @hostname)
+
+      raise 'Multiple DRYs!' if drys.count > 1
+
+      if drys.count == 0
+        @dry = {:hostname => @hostname}
+      else
+        @dry = drys.first
+      end
+
+      @update = {:hostname => @hostname}
     end
-  
+
     def report name, payload
-      @update[name] = payload
+      @update[name] = mongify payload
     end
 
     def wants identifier, dry_payload
-      true
+      orig = @dry[identifier]
+      @dry[identifier] = dry_payload
+
+      return orig == dry_payload
     end
-  
+
     def finish
-      # TODO: This allows stale data inside the sources.
-      @coll.update({:hostname => Socket.gethostname}, @update, :upsert => true)
+      @db['host'].update({:hostname => @hostname}, {'$set' => @update}, :upsert => true)
+      @db['dry'].save @dry
+    end
+
+    # Sad I had to write this...
+    # But the query DSL makes it half-OK.
+    # {"$and": [ {"facter.kernel":"Darwin"}, {"augeas.etc.shells": "/bin/zsh"}]}
+    private
+    def mongify o
+      if o.respond_to? :keys
+        return Hash[o.collect do |k,v|
+          [k.to_s.gsub('.','_').gsub('$','%'), mongify(v)]
+        end]
+      else
+        return o
+      end
     end
   end
 end
