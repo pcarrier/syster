552ea7cb97f309a9d35635ee8c6c9f4317536cd2
Pierre Carrier <pierre@gcarrier.fr>
2013-02-21 01:23:11 +0100

    Sources::LinuxProcesses: collect process counts
    Now we're putting some noise in there. Oh yeah.

---
+++ lib/kolekt/sources/linux_processes.rb
@@ -0,0 +1,33 @@
+require 'kolekt/sources/base'
+require 'shellwords'
+
+module Kolekt::Sources
+  class LinuxProcesses < Base
+    def self.identifier
+      'processes'
+    end
+
+    def self.runnable?
+      # BSDs don't have /proc/$PID
+      File.exists? '/proc/1/cmdline'
+    end
+
+    def collect
+      stats = Hash.new 0
+
+      Dir.entries('/proc').each do |e|
+        # skip non-process entries
+        next unless e =~ /^[0-9]+$/
+
+        begin
+          cmd = File.read("/proc/#{e}/cmdline").split("\0").shelljoin
+          stats[cmd]++
+        rescue Errno::ENOENT
+          # ignore processes disappearing under our feet
+        end
+      end
+
+      return [true, stats]  
+    end
+  end
+end
