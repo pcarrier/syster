00b2d2ad19eb04c22fd1b56f0e559e5316d6d3a4
Pierre Carrier <pierre@gcarrier.fr>
2013-02-21 06:33:24 +0100

    Kolekt::{Reporters,Sources}: support for "local" code
    Should make it reasonably easy to install your own reporters and sources.
    Some support for gems would also be nice.

--- lib/kolekt/reporters.rb
+++ lib/kolekt/reporters.rb
@@ -1,15 +1,30 @@
-Dir[File.join File.dirname(__FILE__), 'reporters', '*.rb'].each {|f| require f}
-
 module Kolekt
   module Reporters
     def self.available
-      @@AVAILABLE ||= Hash[constants.collect do |c|
-        const_get c
-      end.select do |c|
-        c.instance_of? Class and c.identifier != 'base' and c.runnable?
-      end.collect do |c|
-        [c.identifier, c]
-      end]
+      unless defined? @@AVAILABLE
+        load_from_dir local_directory
+        load_from_dir File.join(File.dirname(__FILE__), 'reporters')
+
+        @@AVAILABLE = Hash[constants.collect do |c|
+          const_get c
+        end.select do |c|
+          c.instance_of? Class and c.identifier != 'base' and c.runnable?
+        end.collect do |c|
+          [c.identifier, c]
+        end]
+      end
+
+      return @@AVAILABLE
+    end
+
+    private
+    def self.local_directory
+      ENV['REPORTERS_DIR'] or '/var/lib/kolekt/reporters'
+    end
+
+    private
+    def self.load_from_dir dir
+      Dir[File.join(dir, '*.rb')].each {|f| require f}
     end
   end
 end
--- lib/kolekt/sources.rb
+++ lib/kolekt/sources.rb
@@ -1,13 +1,28 @@
-Dir[File.join File.dirname(__FILE__), 'sources', '*.rb'].each {|f| require f}
-
 module Kolekt
   module Sources
     def self.available
-      @@AVAILABLE ||= constants.collect do |c|
-        const_get(c)
-      end.select do |c|
-        c.instance_of? Class and c.identifier != 'base'
-      end
+      unless defined? @@AVAILABLE
+        load_from_dir local_directory
+        load_from_dir File.join(File.dirname(__FILE__), 'sources')
+
+        @@AVAILABLE = constants.collect do |c|
+          const_get(c)
+        end.select do |c|
+          c.instance_of? Class and c.identifier != 'base'
+        end
+    end
+
+      return @@AVAILABLE
+    end
+
+    private
+    def self.local_directory
+      ENV['SOURCES_DIR'] or '/var/lib/kolekt/sources'
+    end
+
+    private
+    def self.load_from_dir dir
+      Dir[File.join(dir, '*.rb')].each {|f| require f}
     end
   end
 end
